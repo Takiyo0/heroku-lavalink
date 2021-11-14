5562e021ae4c6c3ee25c0b416d7e5023317eb6a6
null <60427892+vierofernando@users.noreply.github.com>
2021-11-14 19:12:28 +0700

    perf(start): no need for class

--- start.py
+++ start.py
@@ -1,14 +1,8 @@
 from os import system
 
-
-class StartLavalink:
-    def run(self):
-        try:
-            system('sed -i "s|DYNAMICPORT|$PORT|" application.yml')
-            system("java -jar Lavalink.jar")
-
-        except BaseException as exc:
-            print(f"[ERROR] Failed to start Lavalink. Info: {exc}")
-
-
-StartLavalink().run()
+if __name__ == "__main__":
+    try:
+        system('sed -i "s|DYNAMICPORT|$PORT|" application.yml')
+        system("java -jar Lavalink.jar")
+    except BaseException as exc:
+        print(f"[ERROR] Failed to start Lavalink. Info: {exc}")
